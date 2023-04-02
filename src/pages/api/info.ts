6818f780b1b11db864c6e95240561a7b07a44fc9
Shridhar Kamat <shridharkamat10@gmail.com>
2023-04-02 17:10:18 +0530

    minor changes

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -2,30 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import puppeteer from 'puppeteer';
 
-function isUpperCase(str:string) {
-  return str === str.toUpperCase();
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
 
-  //let formattedString = req.query.constellation.replace(/[A-Z&]/g, (m,i) => isUpperCase(m) && i!==0  ? "%20"+m.toLowerCase() : m.toLowerCase());
-  
-  /*
-  let formattedString=[];
-
-  for(let i=req.query.constellation.length; i>0; i--){
-    if(isUpperCase(req.query.constellation[i-1])){
-      formattedString[i] = '%20'
-      continue;
-    }
-    formattedString[i] = req.query.constellation[i-1]
-  }
-
-  console.log(formattedString)
-*/
   (async () => {
    
     const browser = await puppeteer.launch();
@@ -37,6 +18,6 @@ export default async function handler(
     let value = await page.evaluate((el:Element | null) => el?.textContent, element)
     res.status(200).json(value || '')
     await browser.close();
- })();
+  })();
     
 }
